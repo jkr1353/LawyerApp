3e18eab91f4f1aa3afcbaf07440353d1e8e76b5d
agerwe <Austin@Austins-MacBook-Pro-22.local>
2013-11-05 06:42:06 -0500

    Taking Photos capability added

--- LawyerApp/src/com/example/lawyerapp/FileFrag.java
+++ LawyerApp/src/com/example/lawyerapp/FileFrag.java
@@ -1,13 +1,17 @@
 package com.example.lawyerapp;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 public class FileFrag extends Fragment {
 
+	ImageButton photoButton;
+	
 	@Override
 	public void onCreate(Bundle saved) {
 		super.onCreate(saved);
@@ -19,7 +23,20 @@ public class FileFrag extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle saved) {
 		View v = inflater.inflate(R.layout.file_frag, parent,false); 
 		
+		photoButton = (ImageButton) v.findViewById(R.id.photobutton);
+		
+		photoButton.setOnClickListener(new View.OnClickListener() 
+		{
 			
+			@Override
+			public void onClick(View v) 
+			{
+				Intent takePhoto = new Intent(getActivity(), PhotoIntentActivity.class);
+				
+				startActivity(takePhoto);
+			}
+			
+		});
 		
 		return v; 
 	}
