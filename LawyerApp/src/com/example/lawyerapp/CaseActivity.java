808d314390d5f2e15ce06db025557fd9340a9410
agerwe <Austin@10.20.216.216>
2013-11-07 17:41:41 -0500

    Newest

--- LawyerApp/src/com/example/lawyerapp/CaseActivity.java
+++ LawyerApp/src/com/example/lawyerapp/CaseActivity.java
@@ -18,7 +18,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class CaseActivity extends FragmentActivity {
 
